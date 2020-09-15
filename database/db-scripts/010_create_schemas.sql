d2a5279dbdeb6e965462cc69ffebc7c02b5bfec5
Davi Diorio Mendes <d.mendes@samsung.com>
2020-09-15 17:12:26 -0300

    Add created and updated at fields to db

--- database/db-scripts/010_create_schemas.sql
+++ database/db-scripts/010_create_schemas.sql
@@ -1,6 +1,6 @@
 
-CREATE TABLE IF NOT EXISTS repository (
-	id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS repositories (
+	id SERIAL,
 	owner TEXT,
 	name TEXT,
 	stars INTEGER,
@@ -8,5 +8,21 @@ CREATE TABLE IF NOT EXISTS repository (
 	subscribers INTEGER,
 	watchers INTEGER,
 	open_issues INTEGER,
-	size INTEGER);
+	size INTEGER,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id, owner, name));
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.updated = NOW();
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON repositories
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
 
